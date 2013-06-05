b06808454cbb98005f31f0942d0c974632499098
Jessica Kerr <jessitron@gmail.com>
2013-06-05 08:29:36 -0500

    horizontal concatenation

--- lib/aqueductron/drawing.rb
+++ lib/aqueductron/drawing.rb
@@ -8,5 +8,39 @@ module Aqueductron
       spaces = " " * symbol.length
       ["\\" + spaces, " #{symbol}", "/" + spaces ]
     end
+
+    def self.horizontal_concat(first, second)
+      # I <3 Ruby. this kind of recursive ref doesn't work in Scala
+      identity = ->(piece,msg) { piece.pass_on(msg, identity)}
+      concat_one_of_these= ->(array) {
+        ->(piece,msg) {
+           (head, *tail) = array
+           if (tail.empty?)
+             piece.pass_on(msg + head, identity)
+           else
+             piece.pass_on(msg + head, concat_one_of_these.call(tail))
+           end
+        }
+      }
+      centeredSecond = centered_on(first.length, second)
+      centered_first = centered_on(second.length, first, padding(first), padding(first))
+      duct = Duct.new.custom(concat_one_of_these.call(centeredSecond)).array()
+      duct.flow(centered_first).value
+    end
+
+    private
+    def self.centered_on(length, array, paddingBefore = "", paddingAfter = "")
+      if (length <= array.length)
+        array
+      else
+        top_spacing = (length - array.length) / 2
+        bottom_spacing = ((length - array.length) / 2.0).ceil
+        ([paddingBefore] * top_spacing) + array + ([paddingAfter] * bottom_spacing)
+      end
+    end
+
+    def self.padding(array)
+      " " * array.first.length
+    end
   end
 end
--- spec/draw_spec.rb
+++ spec/draw_spec.rb
@@ -1,21 +1,22 @@
 require_relative '../lib/aqueductron.rb'
 
-draw_array = ->(a) { a.join("\n") + "\n"}
 
 # how to say 'all draw methods at all times have the property that the
 # string element in each array is the same length' ?
-describe 'The drawing of the pipe' do
-  describe 'An ordinary piece' do
-    it 'should print the default description' do
-      draw_array.call(Aqueductron::Piece.new(:dummy,:dummy).draw).should == <<eos
+module Aqueductron
+  draw_array = ->(a) { a.join("\n") + "\n"}
+  describe 'The drawing of the pipe' do
+    describe 'An ordinary piece' do
+      it 'should print the default description' do
+        draw_array.call(Piece.new(:dummy,:dummy).draw).should == <<eos
 -
 ~
 -
 eos
-    end
+      end
 
     it 'should print a custom description' do
-      draw_array.call(Aqueductron::Piece.new(:dummy,:dummy,"hello").draw).should == <<eos
+      draw_array.call(Piece.new(:dummy,:dummy,"hello").draw).should == <<eos
 -----
 hello
 -----
@@ -26,33 +27,59 @@ eos
   describe 'drawing an end piece' do
     it 'displays the default symbol' do
       # I'd rather have a herefile but my vi deletes trailing spaces!
-      draw_array.call(Aqueductron::EndPiece.new(Aqueductron::Monoid.plus, :dummy).draw).should == "\\ \n +\n/ \n"
+      draw_array.call(EndPiece.new(Monoid.plus, :dummy).draw).should == "\\ \n +\n/ \n"
     end
     #todo: test custom symbol
 
     it 'displays # for counting' do
-      draw_array.call(Aqueductron::CountingEndPiece.new.draw).should == "\\ \n #\n/ \n"
+      draw_array.call(CountingEndPiece.new.draw).should == "\\ \n #\n/ \n"
     end
     it 'displays last for last' do
-      draw_array.call(Aqueductron::LastEndPiece.new.draw).should == "\\    \n last\n/    \n"
+      draw_array.call(LastEndPiece.new.draw).should == "\\    \n last\n/    \n"
 
     end
   end
 
   describe 'drawing a joint piece' do
     it 'has three times as many elements as paths: 1' do
-       Aqueductron::JointPiece.new({ :this => :dummy}).draw.size.should == 3
+       JointPiece.new({ :this => :dummy}).draw.size.should == 3
     end
     it 'looks right: 1' do #TODO: add growy symbol in front
-       draw_array.call(Aqueductron::JointPiece.new({ :this => :dummy}).draw).should == <<eos
+       draw_array.call(JointPiece.new({ :this => :dummy}).draw).should == <<eos
 ----
 this
 ----
 eos
-    end
+      end
     # wish this were a property
-    it 'has three times as many elements as paths: 2' do
-       Aqueductron::JointPiece.new({ :this => :dummy, :that => :dummy}).draw.size.should == 6
+      it 'has three times as many elements as paths: 2' do
+       JointPiece.new({ :this => :dummy, :that => :dummy}).draw.size.should == 6
+      end
+    end
+  end
+  describe 'horizontal concatenation' do
+    it ('can handle the trivial case of just one') do
+      Drawing.horizontal_concat(["blah"],[">"]).should == ["blah>"]
+    end
+    it ('can concatenate two the same length') do
+      Drawing.horizontal_concat(["a","b"],["1","2"]).should == ["a1","b2"]
+    end
+    it ('can concatenate when the second is smaller') do
+      Drawing.horizontal_concat(["a","b"],["1"]).should == ["a1","b"]
+    end
+
+    it ('centers the second when the second is smaller') do
+      Drawing.horizontal_concat(["a","b","c"],["1"]).should == ["a","b1","c"]
+    end
+    it ('centers the second when the second is smaller unevenly') do
+      Drawing.horizontal_concat(["a","b","c","d"],["1"]).should == ["a","b1","c","d"]
+    end
+    it ('handles a smaller first') do
+      Drawing.horizontal_concat(["a"],["1","2"]).should == ["a1"," 2"]
+    end
+    it('handles a much smaller first') do
+      Drawing.horizontal_concat(["1"],["a","b","c","d"]).should == [" a","1b"," c"," d"]
+
     end
   end
 end
