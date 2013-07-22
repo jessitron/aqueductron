d5ae0090fa0dd3f77be02e933afe60c7d744d4ec
Mario Aquino <mario.e.aquino@gmail.com>
2013-07-21 19:30:08 -0500

    Reasonable spacing

--- lib/aqueductron/array_end_piece.rb
+++ lib/aqueductron/array_end_piece.rb
@@ -4,12 +4,15 @@ require_relative 'drawing'
 module Aqueductron
   class ArrayEndPiece
     include PieceCommon
+
     def initialize(so_far = [])
       @so_far = so_far
     end
+
     def eof
       SimpleResult.new(@so_far)
     end
+
     def receive msg
       ArrayEndPiece.new(@so_far + [msg])
     end
