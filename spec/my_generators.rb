34b75054dc2d3e14b6ef9a5dfa9963598b17db25
Jessica Kerr <jessitron@gmail.com>
2014-01-11 23:37:34 -0600

    small integers

--- spec/my_generators.rb
+++ spec/my_generators.rb
@@ -9,12 +9,20 @@ module Generators
   def intAmong(from, to)
     rand(from..to)
   end
+
+  def smallInt(to = 10)
+    intAmong(0, to)
+  end
 end
 
 class GeneratingThing
   include Generators
 end
 
+#
+# Mario: is there some way to include the generator module
+# in my TEST?
+#
 describe Generators do
   subject = GeneratingThing.new
 
@@ -29,7 +37,7 @@ describe Generators do
   end
 
   describe("integers") do
-    generative("numbers in the range")
+    generative("numbers in the range") do
       data(:one) { rand(1000) }
       data(:two) { rand(1000) }
 
@@ -40,6 +48,23 @@ describe Generators do
           (sample <= two).should == true
         end
       end
+    end
+    generative("small integers") do
+      # how can I test that it isn't always returning 3?
+      data(:sample) { subject.smallInt }
+      it("is between 0 and 10 by default") do
+        (sample >= 0).should == true
+        (sample <= 10).should == true
+      end
+      it("never exceeds the bound, if passed in") do
+        newSample = subject.smallInt(sample)
+        (newSample >= 0).should == true
+        (newSample <= sample).should == true
+      end
+    end
+  end
+
+  describe("arrays of something else") do
 
   end
 
