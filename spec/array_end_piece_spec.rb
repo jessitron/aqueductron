a4487179ee56343a0a4afeba4507facc186a4e0d
Jessica Kerr <jessitron@gmail.com>
2014-01-12 00:05:19 -0600

    Add a generative test for array

--- spec/array_end_piece_spec.rb
+++ spec/array_end_piece_spec.rb
@@ -1,13 +1,26 @@
 require_relative '../lib/aqueductron.rb'
+require_relative 'my_generators'
+require 'generative'
 
 module Aqueductron
+  gen = GeneratingThing.new
   describe EndPiece do
+    let(:subject) { Duct.new.array()}
     it ("gives me an empty array for empty input")do
-      Duct.new.array().flow([]).value.should == []
+      subject.flow([]).value.should == []
     end
 
     it ("gives me what I send in an array") do
-      Duct.new.array().flow("jess".chars).value.should == ["j","e","s","s"]
+      subject.flow("jess".chars).value.should == ["j","e","s","s"]
+    end
+
+    generative ("works for random arrays") do
+      data(:input) { gen.arrayOf(gen.method :smallInt) }
+
+      it("spits the input back out") do
+        subject.flow(input).value.should == input
+      end
+
     end
   end
 end
